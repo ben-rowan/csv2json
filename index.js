0da18d9e65cd36bb2eb67b925bc684a70460a209
Ben Rowan <ben.rob.rowan@gmail.com>
2017-04-03 11:44:11 +0100

    Add backtrack parsing functions

--- index.js
+++ index.js
@@ -11,106 +11,124 @@ var os = require('os');
 
 // TOKEN ----------------------------------------------------------------------
 
-function Token(type, text)
-{
+function Token(type, text) {
     this.type = type;
     this.text = text;
 }
 
+Token.types = {};
 Token.types.EOF_TYPE = 0;
 Token.types.DEL_STR_TYPE = 1;
 Token.types.NON_DEL_STR_TYPE = 2;
 Token.types.SEPARATOR_TYPE = 3;
 Token.types.NL_TYPE = 4;
+Token.types.NUMBER_TYPE = 5;
 
 Token.names = [
     'EOF',
     'DELIMITED_STRING',
     'NON_DELIMITED_STRING',
     'SEPARATOR',
-    'NL'
+    '_NL',
+    '_NUMBER'
 ];
 
-Token.prototype.stringify = function ()
-{
+Token.prototype.stringify = function () {
     return '<' + Token.names[this.type] + ',' + this.text + '>';
 };
 
 // LEXER ----------------------------------------------------------------------
 
-function LexerException(msg)
-{
+function LexerException(msg) {
     this.name = 'Lexer Exception';
     this.msg = msg;
 }
 
-function Lexer(input)
+LexerException.prototype.toString = function()
 {
+    return this.name + ': ' + this.msg;
+};
+
+function Lexer(input) {
     this.i = 0;
     this.input = input;
 }
 
-Lexer.prototype.consume = function ()
-{
+Lexer.prototype.consume = function () {
     ++this.i;
 };
 
-Lexer.prototype.c = function ()
-{
+Lexer.prototype.c = function () {
     return this.input[this.i];
 };
 
-// HEADER LEXER ---------------------------------------------------------------
+// CSV LEXER ---------------------------------------------------------------
 
-function HeaderLexer(csv, separator, delimiter, nl)
-{
+function CsvLexer(csv, separator, delimiter, nl) {
     Lexer.call(this, csv);
     this.separator = separator;
     this.delimiter = delimiter;
     this.nl = nl;
 }
 
-HeaderLexer.prototype = Object.create(Lexer.prototype);
-HeaderLexer.prototype.constructor = HeaderLexer;
+CsvLexer.prototype = Object.create(Lexer.prototype);
+CsvLexer.prototype.constructor = CsvLexer;
 
-HeaderLexer.prototype.isNonDelStrChar
+CsvLexer.prototype._NL = function () {
+    this.consume();
+    return new Token(Token.types.NL_TYPE, Token.names[Token.types.NL_TYPE]);
+};
 
-HeaderLexer.prototype.nextToken = function ()
-{
-    while (typeof this.c() != 'undefined') {
-        switch (this.c()) {
-            case this.separator:
-                break;
-            case this.delimiter:
-                break;
-            case this.nl:
-                this.consume();
-                return new Token(Token.types.NL_TYPE, Token.names[Token.types.NL_TYPE]);
-            case ' ': case '\t': case '\n': case '\r': this.consume(); continue;
-            default:
-                if () {
+CsvLexer.prototype._DEL_STRING = function () {
+    var backtrackIndex = this.i;
 
-                }
-                else {
-                    throw new LexerException('Syntax error found at character (' + this.i + ').');
-                }
-        }
+    this.i = backtrackIndex;
+    return false;
+};
+
+CsvLexer.prototype._NON_DEL_STRING = function () {
+    var backtrackIndex = this.i;
+
+    this.i = backtrackIndex;
+    return false;
+};
+
+CsvLexer.prototype._NUMBER = function () {
+    var backtrackIndex = this.i;
+
+    this.i = backtrackIndex;
+    return false;
+};
+
+CsvLexer.prototype.nextToken = function () {
+    if (typeof this.c() === 'undefined') {
+        return new Token(Token.types.EOF_TYPE, Token.names[Token.types.EOF_TYPE]);
     }
 
-    return new Token(Token.types.EOF_TYPE, Token.names[Token.types.EOF_TYPE]);
+    switch (this.c()) {
+        case this.nl:
+            return this._NL();
+        default:
+            var token = null;
+            if ( (token = this._DEL_STRING()) !== false ) {}
+            else if ( (token = this._NUMBER()) !== false ) {}
+            else if ( (token = this._NON_DEL_STRING()) !== false ) {}
+            else {
+                throw new LexerException('Syntax error found at character (' + this.i + ').');
+            }
+            return token;
+    }
 };
 
 // PARSER ---------------------------------------------------------------------
 
-function Parser()
-{
+function Parser() {
 
 }
 
 // HEADER PARSER --------------------------------------------------------------
 
-function HeaderParser()
-{
+function HeaderParser() {
 
 }
 
@@ -118,19 +136,26 @@ HeaderParser.prototype = Object.create(Parser.prototype);
 HeaderParser.prototype.constructor = HeaderParser;
 
 program
-  .arguments('<file>')
-  .option('-t, --tab', 'switches the separator from a comma to a tab')
-  .option('-d, --delimiter <value>', 'sets the symbol used to delimit text [default "]')
-  .option('-h, --header', 'this CSV file includes a header')
-  .option('-w, --windows-nl', 'sets the newline to "\\r\\n" instead of "\\n"')
-  .action(function(file) {
+    .arguments('<file>')
+    .option('-t, --tab', 'switches the separator from a comma to a tab')
+    .option('-d, --delimiter <value>', 'sets the symbol used to delimit text [default "]')
+    .option('-h, --header', 'this CSV file includes a header')
+    .option('-w, --windows-nl', 'sets the newline to "\\r\\n" instead of "\\n"')
+    .action(function (file) {
 
-    fs.readFile(file, 'utf8', function(err, csv) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
+        fs.readFile(file, 'utf8', function (err, csv) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
 
-    });
-  })
-  .parse(process.argv);
+            var lexer = new CsvLexer(csv, ',', '"', '\n');
+            var token = null;
+            while (true) {
+                token = lexer.nextToken();
+                if (token.type === Token.types.EOF_TYPE) break;
+            }
+            process.stdout.write('DONE!');
+        });
+    })
+    .parse(process.argv);
